338540f6bf9d215996e17fa299abf980288e84df
Zakes <134063053+ZakheleNdlovu@users.noreply.github.com>
2024-08-07 03:23:31 +0100

    Update README.js

---
+++ README.js
@@ -0,0 +1,7 @@
+let me = {
+	name: "Zakhele Ndlovu",
+	age: 24,
+	code:["JavaScript","Python","C++","HTML & CSS","MySQL"],
+	hobbies: ["Progtamming","Drawing","Watching & Playing Basketball","Video Games"],
+	tools: ["Python","VS Code","HTML & CSS","JavaScript","Jupyter Notebook","MySQL Workbench/shell"]
+}
