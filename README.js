777ba62f42b52cb51cdd3a269118308c3bfdb5ba
Zakhele Ndlovu <134063053+ZakheleNdlovu@users.noreply.github.com>
2025-07-12 06:47:34 +0200

    Added linktree

--- README.js
+++ README.js
@@ -6,6 +6,7 @@
 	},
 	platforms: ["Python", "JavaScript", "HTML", "CSS", "React native/Next.js" ],
         database: ["MySQL", "MongoDB",
-        tools: ["Microsoft Office", "Visual Studio Code", "MySQL workbench/shell", "Pycharm", "Amazon Web Services"]
+        tools: ["Microsoft Office", "Visual Studio Code", "MySQL workbench/shell", "Pycharm", "Amazon Web Services"],
+        links : ["linktr.ee/umzakesman"],
 	
 }
