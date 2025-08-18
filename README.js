ea685f189a06920df83110760c31038bc9ff1d8f
Zakhele Ndlovu <134063053+ZakheleNdlovu@users.noreply.github.com>
2025-08-18 02:38:06 +0100

    Update README.js

--- README.js
+++ README.js
@@ -1,15 +1,45 @@
 
-```Javascript
-{
-	about:{
-		name: "Zakhele",
-		surname: "Ndlovu",
-		age: 25,
-	},
-	platforms: ["Python", "JavaScript", "HTML", "CSS", "React native/Next.js" ],
-        database: ["MySQL", "MongoDB",],
-        tools: ["Microsoft Office", "Visual Studio Code", "MySQL workbench/shell", "Pycharm", "Amazon Web Services"],
-        links : ["linktr.ee/umzakesman"],
-	
-}
-```
+# Hi there, I'm Zakhele Ndlovu 👋
+
+Welcome to my GitHub profile!  
+I'm a passionate software developer, problem solver, and lifelong learner. I love exploring new technologies, building innovative solutions, and collaborating with others in the tech community.
+
+## 🚀 About Me
+
+- 🔭 I’m currently working on: _[Your current project or area of focus]_
+- 🌱 I’m learning: _[Technologies or skills you’re learning]_
+- 👯 I’m looking to collaborate on: _[Types of projects or technologies]_
+- 💬 Ask me about: _[Your key skills, technologies, or favorite topics]_
+- 📫 How to reach me: _[Your email or preferred contact method]_
+- ⚡ Fun fact: _[A fun, unique fact about you!]_
+
+## 🛠️ Languages & Tools
+
+![JavaScript](https://img.shields.io/badge/-JavaScript-black?style=flat-square&logo=javascript)
+![Python](https://img.shields.io/badge/-Python-3776AB?style=flat-square&logo=python&logoColor=white)
+![React](https://img.shields.io/badge/-React-20232a?style=flat-square&logo=react)
+![Node.js](https://img.shields.io/badge/-Node.js-43853d?style=flat-square&logo=node-dot-js)
+<!-- Add or remove badges according to your stack -->
+
+## 📈 GitHub Stats
+
+<p align="center">
+  <img src="https://github-readme-stats.vercel.app/api?username=ZakheleNdlovu&show_icons=true&theme=radical" alt="Zakhele Ndlovu's GitHub stats" />
+</p>
+
+## 📝 Latest Projects
+
+- [Project 1](https://github.com/ZakheleNdlovu/full-time) - football scores and standings.
+- [Project 2](https://github.com/ZakheleNdlovu/the-cook-book) - Foot recipes from around the world at your fingertips.
+<!-- List your best or most relevant repos here -->
+
+## 🌐 Connect with Me
+
+[![LinkedIn](https://img.shields.io/badge/-LinkedIn-0077B5?style=flat-square&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/zakhele-ndlovu-813961174)
+[![Twitter](https://img.shields.io/badge/-Twitter-1da1f2?style=flat-square&logo=twitter&logoColor=white)](https://x.com/ZakheleNdlovu)
+[![Email](https://img.shields.io/badge/-Email-c14438?style=flat-square&logo=Gmail&logoColor=white)](ndlovuz790@gmail.com)
+<!-- Add your social links -->
+
+---
+
+Thank you for visiting my profile! 🚀
