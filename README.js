1497870b5c3c12a9ed610155179dd8a0da2e3bdb
Zakhele Ndlovu <134063053+ZakheleNdlovu@users.noreply.github.com>
2025-08-19 03:18:19 +0100

    Update README.js

--- README.js
+++ README.js
@@ -13,29 +13,24 @@ I'm a passionate software developer, problem solver, and lifelong learner. I lov
 
 ## 🛠️ Languages & Tools
 
-![JavaScript](https://img.shields.io/badge/-JavaScript-black?style=flat-square&logo=javascript)
-![Python](https://img.shields.io/badge/-Python-3776AB?style=flat-square&logo=python&logoColor=white)
-![React](https://img.shields.io/badge/-React-20232a?style=flat-square&logo=react)
-![Node.js](https://img.shields.io/badge/-Node.js-43853d?style=flat-square&logo=node-dot-js)
+- JavaScript
+- Python
+- React
+- Node.js
+- c++
 
-
-## 📈 GitHub Stats
-
-<p align="center">
-  <img src="https://github-readme-stats.vercel.app/api?username=ZakheleNdlovu&show_icons=true&theme=radical" alt="Zakhele Ndlovu's GitHub stats" />
-</p>
 
 ## 📝 Latest Projects
 
-- [Project 1](https://github.com/ZakheleNdlovu/full-time) - football scores and standings.
-- [Project 2](https://github.com/ZakheleNdlovu/the-cook-book) - Foot recipes from around the world at your fingertips.
+- Project 1 (https://github.com/ZakheleNdlovu/full-time) - football scores and standings.)
+- Project 2 (https://github.com/ZakheleNdlovu/the-cook-book) - Foot recipes from around the world at your fingertips.)
 
 
 ## 🌐 Connect with Me
 
-[![LinkedIn](https://img.shields.io/badge/-LinkedIn-0077B5?style=flat-square&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/zakhele-ndlovu-813961174)
-[![Twitter](https://img.shields.io/badge/-Twitter-1da1f2?style=flat-square&logo=twitter&logoColor=white)](https://x.com/ZakheleNdlovu)
-[![Email](https://img.shields.io/badge/-Email-c14438?style=flat-square&logo=Gmail&logoColor=white)](ndlovuz790@gmail.com)
+- LinkedIn - (https://www.linkedin.com/in/zakhele-ndlovu-813961174)
+- Twitter - (https://x.com/ZakheleNdlovu)
+- Email] - (ndlovuz790@gmail.com)
 
 
 ---
